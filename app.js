043d2122b378b3c2b0b5ce31eb8ec5aa6b08ff11
ChenChiaChi <chenchiachi.ccc@gmail.com>
2021-08-14 22:14:40 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const app = express()
+const Todo = require('./models/todo')
 const port = 3000
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection 
-db.on= ('error', () => {
-  console.log('mongodb error!')
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => {
+  console.log('mongodb error!') // 連線異常
 })
 db.once('open', () => {
-  console.log('mongodb connected!')
+  console.log('mongodb connected!') // 連線成功
 })
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error))
 })
 
 app.listen(port, () => {
