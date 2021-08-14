fb79498f15bf5fb9d1e3383a0484780766a35802
ChenChiaChi <chenchiachi.ccc@gmail.com>
2021-08-14 17:15:10 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection 
+db.on= ('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('This is todo-list project.')
 })
 
 app.listen(port, () => {
-  console.log(`The express server is runnung on http://localhost:${port}`)
+  console.log(`The express server is running on http://localhost:${port}`)
 })
