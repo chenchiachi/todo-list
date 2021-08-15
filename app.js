f52ad438b894bc23e1e297e3ad99a21ac8891fdf
ChenChiaChi <chenchiachi.ccc@gmail.com>
2021-08-15 17:18:26 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const app = express()
+const bodyParser = require('Body-Parser')
+
 const Todo = require('./models/todo')
+
+const app = express()
 const port = 3000
 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,11 +19,25 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error))
+})
+
+app.get('/todos/new', (req, res) => {
+   return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+  .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
